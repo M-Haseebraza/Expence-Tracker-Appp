2e74cb8c524fae78fda00747a8312c09340fc284
M.Haseebraza <haseeb396raza@gmail.com>
2021-02-09 22:42:29 +0500

    Add button for check surge working

--- src/components/Transaction/amount.js
+++ src/components/Transaction/amount.js
@@ -2,10 +2,11 @@ import React ,{useRef} from 'react';
 import './transaction.css';
 export default function Amount(){
    let myRef=useRef();
-   console.log(myRef.current);
+      console.log(myRef.current);
    return( <>
     <label>Add Amount</label>
     <input ref={myRef} type='text' placeholder='Enter Amount'/>
+    <button>Hi</button>
 
     </>
    )
