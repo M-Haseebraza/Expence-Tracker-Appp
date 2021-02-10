37d3fd482d2948ed76a30b31ff690066ea2dd8ba
M.Haseebraza <haseeb396raza@gmail.com>
2021-02-10 22:36:40 +0500

    clear console

--- src/components/Transaction/amount.js
+++ src/components/Transaction/amount.js
@@ -2,7 +2,6 @@ import React ,{useRef} from 'react';
 import './transaction.css';
 export default function Amount(){
    let myRef=useRef();
-      console.log(myRef.current);
    return( <>
     <label>Add Amount</label>
     <input ref={myRef} type='text' placeholder='Enter Amount'/>
